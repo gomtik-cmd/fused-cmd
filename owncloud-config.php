c57b2e1c3fb013ebbb5e35f31cc225888e716222
Chaitnaya L Gamdetti <chalogamde@gmail.com>
2021-10-02 16:07:48 +0530

    Sample config for owncloud setup

---
+++ owncloud-config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3l3fs9wxkk',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.5',
+  ),
+  'passwordsalt' => 'rL2SGkVGvyieYHooOQPiHiJfboS9OB',
+  'secret' => '24PaWhU/X2jPFVX9fl89KzMEdBzoLE766ugVk1WALAaAlVRk',
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://192.168.1.5',
+  'dbtype' => 'sqlite3',
+  'version' => '10.0.10.4',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
